f4f1df3a5a286bb812a6936e6928b2b8cd680f57
Henry Nichols <henry.patch@gmail.com>
2014-02-07 21:14:19 -0600

    Initial commit

---
+++ lib/parser.rb
@@ -0,0 +1,117 @@
+class Parser
+	def initialize(file_name)
+    @file = File.open(file_name, 'r+')
+    @configuration = {}
+    parse_file
+  end
+
+  def get_string(section, key)
+    get_value(section, key).to_s
+  end
+
+  def get_integer(section, key)
+    Integer(get_value(section, key))
+  end
+
+  def get_float(section, key)
+    Float(get_value(section, key))
+  end
+
+  def set_string(section, key, value)
+    set_value(section, key, value.to_s)
+  end
+
+  def set_integer(section, key, value)
+    set_value(section, key, Integer(value))
+  end
+
+  def set_float(section, key, value)
+    set_value(section, key, Float(value))
+  end
+
+  private
+
+  def get_value(section, key)
+    @configuration[section][key]
+  end
+
+  def set_value(section, key, value)
+    @configuration[section] ||= {}
+    @configuration[section][key] = value
+    write_file
+    value
+  end
+
+  def build_output
+    output = ""
+    @configuration.each do |section, keys|
+      output << "[#{section}]\n"
+      keys.each do |key, value|
+        output << "#{key} : #{value}\n"
+      end
+    end
+    output
+  end
+
+  def write_file
+    @file.rewind
+    output = build_output
+    @file.write(output)
+  end
+
+  def parse_file    
+    @file.readlines.each_with_index do |line, index|
+      if match = check_section(line)
+        set_section(match)
+      elsif match = check_key_value(line)
+        set_key_value(match)
+      elsif match = check_value_continuation(line)
+        set_value_continuation(match)
+      elsif not check_blank_line(line)
+        raise_exception
+      end
+    end
+  end
+
+  def check_section(line)
+    line.match /^\[((\s*\S)+)\s*\]\s*$/
+  end
+
+  def check_key_value(line)
+    line.match /^(?<key>(\w+\s*)+)\s*:\s*(?<value>(\S+\s*)+)\s*$/
+  end
+
+  def check_value_continuation(line)
+    line.match /^\s+(?<value>(\S+\s*)+)\s*$/
+  end
+
+  def check_blank_line(line)
+    line.match /^\s*$/
+  end
+
+  def set_section(match)
+    @current_section = match[1].strip
+    @configuration[@current_section] = {}
+  end
+
+  def set_key_value(match)
+    if @current_section
+      @current_key = match[:key].strip
+      @configuration[@current_section][@current_key] = match[:value].strip
+    else
+      raise_exception
+    end
+  end
+
+  def set_value_continuation(match)
+    if @current_key
+      @configuration[@current_section][@current_key] += " #{match[:value].strip}"
+    else
+      raise_exception
+    end
+  end
+
+  def raise_exception(message = "Parser error")
+    raise Exception.new(message)
+  end
+end
---
+++ spec/parser_spec.rb
@@ -0,0 +1,111 @@
+require "rspec"
+require_relative "../lib/parser"
+
+SPEC_DIR = File.expand_path('../', __FILE__)
+
+describe Parser do 
+  before(:all) { FileUtils.copy(File.join(SPEC_DIR, 'configuration.conf.test') , File.join(SPEC_DIR, 'test_configuration.conf')) }
+
+	let(:parser) { Parser.new(File.join(SPEC_DIR, "test_configuration.conf")) }
+
+	context "#initialize" do
+		it "creates a Parser object" do
+			parser.should be_an_instance_of Parser
+		end
+
+		it "requires one parameter" do
+			expect { Parser.new }.to raise_error(ArgumentError)
+		end
+
+	end
+
+	context "#get_string" do
+		it "requires two parameters" do
+			expect { parser.get_string("header") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the string value" do
+			parser.get_string("header", "project").should eq "Programming Test"
+		end
+
+		it "returns a String object" do
+			string_parser = parser.get_string("header", "project")
+			string_parser.should be_an_instance_of String
+		end
+	end
+
+	context "#get_integer" do
+		it "requires two parameters" do
+			expect { parser.get_integer("header") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the integer value" do
+			parser.get_integer("header", "accessed").should eq 205 #string quotes here?
+		end
+
+		it "returns an Integer object" do
+			integer_parser = parser.get_integer("header", "accessed")
+			integer_parser.should be_an_instance_of Fixnum
+		end
+	end
+
+	context "#get_float" do
+		it "requires two parameters" do
+			expect { parser.get_float("header") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the float value" do
+			parser.get_float("header", "budget").should eq 4.5 # string quotes here?
+		end
+
+		it "returns a Float object" do
+			float_parser = parser.get_float("header", "budget")
+			float_parser.should be_an_instance_of Float
+		end
+	end
+
+	context "#set_string" do
+		it "requires three parameters" do
+			expect { parser.set_string("trailer", "budget") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the string value" do
+			parser.set_string("header", "project", "Lonely Planet test").should eq "Lonely Planet test" 
+		end
+
+		it "returns a String object" do
+			string_setter = parser.set_string("header", "project", "Lonely Planet test")
+			string_setter.should be_an_instance_of String
+		end
+	end
+
+	context "#set_integer" do
+		it "requires three parameters" do
+			expect { parser.set_integer("header", "accessed") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the Fixnum value" do
+			parser.set_integer("header", "accessed", 187).should eq 187
+		end
+
+		it "returns an Integer object" do
+			integer_setter = parser.set_integer("header", "accessed", 187)
+			integer_setter.should be_an_instance_of Fixnum
+		end
+	end
+
+	context "#set_float" do
+		it "requires three parameters" do
+			expect { parser.set_float("header", "budget") }.to raise_error(ArgumentError)
+		end
+
+		it "returns the float value" do
+			parser.set_float("header", "budget", 3.7).should eq 3.7
+		end
+
+		it "returns a Float object" do
+			float_setter = parser.set_float("header", "budget", 3.7)
+			float_setter.should be_an_instance_of Float
+		end
+	end
+end
